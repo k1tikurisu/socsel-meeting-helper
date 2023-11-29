54ec99cbdeacfb69a31c39c5766722a47f483ca1
Daiki Maekawa <daaikii@outlook.jp>
2023-11-29 14:42:38 +0900

    修正

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,17 +1,53 @@
 export const kanbans = {
-  石岡: "https://github.com/orgs/Wakayama-SocSEL/projects/18",
-  上中: "https://github.com/orgs/Wakayama-SocSEL/projects/22",
-  大森: "https://github.com/orgs/Wakayama-SocSEL/projects/28",
-  亀岡: "https://github.com/orgs/Wakayama-SocSEL/projects/36",
-  岡本: "https://github.com/orgs/Wakayama-SocSEL/projects/37",
-  前川: "https://github.com/orgs/Wakayama-SocSEL/projects/34",
-  白﨑: "https://github.com/orgs/Wakayama-SocSEL/projects/33",
-  原口: "https://github.com/orgs/Wakayama-SocSEL/projects/25",
-  野口: "https://github.com/orgs/Wakayama-SocSEL/projects/42",
-  飯田: "https://github.com/orgs/Wakayama-SocSEL/projects/41",
-  川﨑: "https://github.com/orgs/Wakayama-SocSEL/projects/43",
-  赤松: "https://github.com/orgs/Wakayama-SocSEL/projects/45",
-  堀尾: "https://github.com/orgs/Wakayama-SocSEL/projects/44"
+  石岡: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/18",
+    level: 4
+  },
+  上中: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/22",
+    level: 3
+  },
+  大森: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/28",
+    level: 3
+  },
+  亀岡: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/36",
+    level: 2
+  },
+  岡本: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/37",
+    level: 2
+  },
+  前川: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/34",
+    level: 2
+  },
+  白﨑: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/33",
+    level: 2
+  },
+  原口: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/25",
+    level: 2
+  },
+  野口: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/42",
+    level: 1
+  },
+  飯田: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/41",
+    level: 1
+  },
+  川﨑: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/43",
+    level: 1
+  },
+  赤松: {
+    url: "https://github.com/orgs/Wakayama-SocSEL/projects/45",
+    level: 1
+  },
+  堀尾: { url: "https://github.com/orgs/Wakayama-SocSEL/projects/44", level: 1 }
 }
 
 export const allLabKanaban =
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,42 +3,49 @@ import "@/styles/style.css"
 import { allLabKanaban, kanbans } from "@/constants/constants"
 import {
   useCallback,
-  useEffect,
   useState,
   type ChangeEventHandler,
-  type MouseEventHandler
+  type FormEventHandler
 } from "react"
 
 function IndexPopup() {
-  const [names, setNames] = useState<string[]>([])
+  const [names, setNames] = useState<Array<keyof typeof kanbans>>([])
 
-  const onOpenKanbansHandler: MouseEventHandler<HTMLButtonElement> =
+  const onOpenKanbansHandler: FormEventHandler<HTMLFormElement> =
     useCallback(() => {
-      chrome.tabs.create({ url: allLabKanaban })
-
       names.forEach((name) => {
-        if (name in kanbans) {
-          chrome.tabs.create({ url: kanbans[name] })
-        }
+        chrome.tabs.create({ url: kanbans[name].url })
       })
+
+      chrome.tabs.create({ url: allLabKanaban })
     }, [names])
 
   const onChangeHandler: ChangeEventHandler<HTMLTextAreaElement> = useCallback(
     (e) => {
       const names = e.target.value
         .split(/\s+/)
-        .filter((name) => name in kanbans)
+        .filter((name): name is keyof typeof kanbans => name in kanbans)
+        .sort((a, b) => kanbans[b].level - kanbans[a].level)
       setNames(names)
     },
     []
   )
 
   return (
-    <div className="h-96 w-96 border-black bg-white">
-      <div>
-        <textarea onChange={onChangeHandler} />
-        <button onClick={onOpenKanbansHandler}>おしてね</button>
-      </div>
+    <div className="w-80 border-black bg-white">
+      <form className="mx-auto px-8 py-4" onSubmit={onOpenKanbansHandler}>
+        <textarea
+          onChange={onChangeHandler}
+          id="message"
+          rows={4}
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          placeholder="ここに適当にペースト"></textarea>
+        <button
+          type="submit"
+          className="mt-2 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">
+          かんばんを開く
+        </button>
+      </form>
     </div>
   )
 }
